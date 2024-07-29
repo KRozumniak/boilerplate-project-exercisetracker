cc6476bba956918b4cd4bc1d13efed4eb92bb83d
Kyrylo Rozumniak <kirkterra@gmail.com>
2024-07-29 00:29:05 -0400

    Updated queries

--- utils/utils.js
+++ utils/utils.js
@@ -1,41 +1,10 @@
 import { format } from 'date-fns-tz';
 
 const defaultDateFormat = 'yyyy-MM-dd';
+export const maxLimit = 1000;
 
 export function getFormattedCurrentDate() {
   const date = new Date();
   const formattedDate = format(date, defaultDateFormat);
   return formattedDate;
-}
-
-export function filterLogsByDate(logs = [], { from, to }) {
-  if (!from && !to) {
-    return logs;
-  }
-
-  if (from && to) {
-    const filteredLogs = logs.filter((log) => {
-      return (
-        new Date(log.date) >= new Date(from) &&
-        new Date(log.date) <= new Date(to)
-      );
-    });
-    return filteredLogs;
-  }
-
-  if (from && !to) {
-    const filteredLogs = logs.filter((log) => {
-      return new Date(log.date) >= new Date(from);
-    });
-    return filteredLogs;
-  }
-
-  if (!from && to) {
-    const filteredLogs = logs.filter((log) => {
-      return new Date(log.date) <= new Date(to);
-    });
-    return filteredLogs;
-  }
-
-  return logs;
 }
